11bc0f6f6dd7657e2c5d2d451b20a1cb097b83f1
Patrick King <pat.k195@gmail.com>
2024-02-29 15:09:59 -0500

    added ability to save a frame state and it displays the x, y coordinates of that save state

---
+++ Base_Model.py
@@ -0,0 +1,1 @@
+# Stores base model
--- main.py
+++ main.py
@@ -5,15 +5,36 @@ import cv2
 from ultralytics import YOLO
 import numpy as np
 import pprint
+from pydantic import BaseModel
+
+# Local Imports
+import Base_Model
 
 # Static Variables
 saved_frames = []
 
-## Functions
-# Function to plot boxes
+# Base Model Class from pydantic
+class GetKeypoint(BaseModel):
 
+    NOSE:           int = 0
+    LEFT_EYE:       int = 1
+    RIGHT_EYE:      int = 2
+    LEFT_EAR:       int = 3
+    RIGHT_EAR:      int = 4
+    LEFT_SHOULDER:  int = 5
+    RIGHT_SHOULDER: int = 6
+    LEFT_ELBOW:     int = 7
+    RIGHT_ELBOW:    int = 8
+    LEFT_WRIST:     int = 9
+    RIGHT_WRIST:    int = 10
+    LEFT_HIP:       int = 11
+    RIGHT_HIP:      int = 12
+    LEFT_KNEE:      int = 13
+    RIGHT_KNEE:     int = 14
+    LEFT_ANKLE:     int = 15
+    RIGHT_ANKLE:    int = 16
 
-# Use existing model
+# Use existing model for pose
 model = YOLO('yolov8n-pose.pt')
 
 ## Video capture
@@ -21,8 +42,8 @@ model = YOLO('yolov8n-pose.pt')
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Set resolution
-cap.set(3, 480) # Width
-cap.set(4, 270) # Height
+cap.set(3, 960) # Width
+cap.set(4, 540) # Height
 
 # Ensures video is opened correctly
 assert cap.isOpened()
@@ -54,11 +75,6 @@ while cap.isOpened():
     # Use frame to determine model annotation
     annotation = model.predict(frame, classes=0)
     
-    # Create list of results object
-    saved_frames.append(model.predict(frame))
-        
-        
-
     # Overaly the annotation onto the webcam view
     annotated_frame = annotation[0].plot(labels=True, boxes=False, conf=1.0)
     
@@ -81,6 +97,10 @@ while cap.isOpened():
         print("Recording stopped")
         index += 1
 
+    # Press 1 to save a frame state and add it to the saved frames list
+    elif key == ord('1'):
+        saved_frames.append(model.predict(frame))
+
     # Write annotated frames to the output video if recording
     if recording:
         out.write(annotated_frame)
@@ -88,7 +108,8 @@ while cap.isOpened():
     # Exit the camera view
     if key == ord('q'):
         break
-
+    
+    #continue loop
     index += 1
 
 # Release the video capture object and close all windows
@@ -97,7 +118,31 @@ if recording:
 cap.release()
 cv2.destroyAllWindows()
 
-pprint.pprint(saved_frames[20][0].keypoints.xyn)
-    
 
+# Loop through saved frames and get keypoints for frames that are imported into numpy array
+for frm in saved_frames:
+
+    # If the information at the object 'frm' is equal to 'keypoints'
+    if hasattr(frm[0], 'keypoints'):
+
+        # local variable to store all keypoints
+        keypoints = frm[0].keypoints
+
+        #local variable to turn keypoints into a readable numpy array
+        keypoints_numpy = keypoints.xyn.cpu().numpy()[0]
+
+        #New instance of GetKeypoint
+        get_keypoint = GetKeypoint()
+
+        #Get nose coordinates
+        nose_x, nose_y = keypoints_numpy[get_keypoint.NOSE]
+
+        #print nose coordinates
+        pprint.pprint(f'X: {nose_x} Y: {nose_y}')
+    else:
+        print('Didnt Work')
+
+        
+
+    
 
